80464b8706f40b056c65cc5922f34c7d7f9c93b9
erhic <ericngugi24@gmail.com>
2023-05-30 14:17:12 +0300

    added a pop up  method used to remopve the last element in an array, and shift to remove the first element in array, both dont take args

--- js_Array_Methods/test.js
+++ js_Array_Methods/test.js
@@ -5,7 +5,7 @@ console.log(myCart[myCart.length-1]) // last element in the array
 console.log(myCart.length=3) //  to change the length of the array
 console.log(myCart)  // the values get update , will only display 3 elements
 
-//indexOf -returns index position (index in  array begins from 0)
+//indexOf -returns index position (index in  array begins from 0) ,this function is case sensetive
 console.log(myCart.indexOf('lemon'))  //-1  this element was removed 
 console.log(myCart.indexOf('carrots'))  //-1  this element was removed 
 console.log(myCart.indexOf('mangos'))  // 0
@@ -21,3 +21,16 @@ console.log('\n')
 console.log(myCart.includes('lemon'));
 console.log('\n')
 myCart.includes('lemons')?console.log( 'item already added in the cart'):console.log('item  added successfully');
+
+
+//pop - used to remove last element from an array
+let userDetails = ['Mrs','Joan', 'Katerina', 23]
+console.log(userDetails); //['Mrs', 'Joan', 'Katerina', 23]
+console.log(userDetails.pop()) //23 
+console.log(userDetails)// [ 'Mrs','Joan','Katerina'] , 23 , was deleted by pop method
+console.log(userDetails.toString()+' Signed up Successfuly ') //Mrs Joan,Katerina
+
+// shift -used to remove first element in an array
+console.log(userDetails.shift()) //Mrs   -Logs the item that has been removed
+console.log(userDetails) //['Joan', 'Katerina']
+console.log(userDetails.toString()) //Joan,Katerina
