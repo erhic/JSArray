10cfc4b817a5e26571b342b2cf35a005e8f054bd
erhic <ericngugi24@gmail.com>
2023-06-01 00:47:03 +0300

    added a forEach to loop through every element of the array , show the the propertie; the item ,index and array, remains in it original state , use a anonymous function

--- js_Array_Methods/Array-Iterators/testIterators.js
+++ js_Array_Methods/Array-Iterators/testIterators.js
@@ -1,2 +1,42 @@
 
 let myCart= [ 'mangos', 'apple', 'jinja','lemon','carrots'];
+
+//forEach..................................................
+
+// the above iterator takes 3 parameters , param-1 is compulsory , param-2 and param-3 are optional.
+// the variables name passed as parameters are dynamic varibles can be changed.
+//uses anonymous  fuction
+
+myCart.forEach(
+  function(value, index, array){
+    console.log(`${value}`)
+   return console.log(`The value is ${value} at index ${index}`)
+  }
+)
+
+
+
+//output
+
+/* mangos
+ The value is mangos at index 0
+ apple
+ The value is apple at index 1
+ jinja
+ The value is jinja at index 2
+ lemon
+ The value is lemon at index 3
+ carrots
+ The value is carrots at index 4........................*/
+
+// map()
+// act the same as forEach array , the difference is that it can create a copy of the original array , can manupilate the array and return different values.
+//using string
+
+function convertToUpperCase(value){
+  return value.toUpperCase();
+}
+
+let myCapsWord = myCart.map(convertToUpperCase) //uses a named function
+console.log(myCapsWord); // ['MANGOS', 'APPLE', 'JINJA', 'LEMON', 'CARROTS'] capitalized,changed from original state, a copy
+console.log(myCart); //  ['mangos', 'apple', 'jinja', 'lemon', 'carrots'] unchanged , in its original state
